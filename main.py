1fd25b7020ac33d792d4c6c34fd5e4821aadaac5
gitwes <wesleyfreire1707@gmail.com>
2024-02-24 22:30:51 -0300

    Listando os diretorios

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 """Importando"""
 
 import PyPDF2
+import os
+
+"""merger é um mesclador"""
+merger = PyPDF2.PdfMerger()
+
+"""lista os diretorios"""
+lista_arquivos = os.listdir("arquivos")
+print(lista_arquivos)
