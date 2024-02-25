7bc2af9bdafc153d2c16480fa35310a5bf825bce
gitwes <wesleyfreire1707@gmail.com>
2024-02-24 22:40:23 -0300

    Finalizado

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ merger = PyPDF2.PdfMerger()
 
 """lista os diretorios"""
 lista_arquivos = os.listdir("arquivos")
+""".sort para ordernar"""
+lista_arquivos.sort()
 print(lista_arquivos)
+
+"""Criando uma condição"""
+for arquivo in lista_arquivos:
+    if ".pdf" in arquivo:
+        merger.append(f"arquivos/{arquivo}")
+
+
+"""salvando """
+merger.write("PDF final.pdf")
